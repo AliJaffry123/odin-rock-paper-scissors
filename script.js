4494f6c76ed4df1df8034f66fe48f6f377ef8fc6
AliJaffry123 <muhammadalijaffry123@gmail.com>
2025-08-21 23:16:24 +0500

    add PlayRound() and PlayGame() game is completed with 5 rounds between human and computer. winner gets 1 point per round. a tie results in 0.5 points each. player with higher points after completion of 5 rounds is the winner of the game.

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
-function getComputerChoice(){
-    number = Math.random()
+function getComputerChoice() {
+    let number = Math.random();
 
-    if(number >= 0 && number < 0.33){
-        return "rock"
-    }
-    else if(number >= 0.33 && number < 0.66){
-        return "paper"
-    }
-    else if(number >= 0.66){
-        return "scissors"
+    if (number < 0.33) {
+        return "rock";
+    } else if (number < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
     }
 }
 
-function getHumanChoice(){
-    answer = prompt("Enter rock , paper or scissors to play")
-    return answer
+function getHumanChoice() {
+    let answer = prompt("Enter rock, paper or scissors to play");
+    return answer.toLowerCase();
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You Win! Rock beats Scissors, You Get 1 point!");
+        return 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You Win! Scissors beats Paper, You Get 1 point!");
+        return 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You Win! Paper beats Rock, You Get 1 point!");
+        return 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You Lose! Rock beats Scissors, Computer Gets 1 point!");
+        return 0;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You Lose! Scissors beat Paper, Computer Gets 1 point!");
+        return 0;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You Lose! Paper beats Rock, Computer Gets 1 point!");
+        return 0;
+    } else if (humanChoice === computerChoice) {
+        console.log("Its a TIE!, both Human and Computer Get 0.5 point each!");
+        return -1;
+    }
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log("Lets Start Round " + i);
+
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        let number = playRound(humanChoice, computerChoice);
+
+        if (number === 1) {
+            humanScore++;
+        } else if (number === 0) {
+            computerScore++;
+        } else if (number === -1) {
+            humanScore += 0.5;
+            computerScore += 0.5;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You have WON the Game!");
+    } else if (humanScore < computerScore) {
+        console.log("Sorry! You have LOST the Game!");
+    } else {
+        console.log("The Game is a TIE!");
+    }
+
+    console.log("Final Score :\nHuman : " + humanScore + "\nComputer : " + computerScore);
+}
+
+playGame();

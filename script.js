205ec21494daacb106f1624fa65955078a3b5d7f
AliJaffry123 <muhammadalijaffry123@gmail.com>
2025-08-21 22:20:41 +0500

    add getComputerChoice() and getHumanChoice() to script.js functions which return the selected choice between rock , paper and scissors by computer and human both for the playing round

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+function getComputerChoice(){
+    number = Math.random()
+
+    if(number >= 0 && number < 0.33){
+        return "rock"
+    }
+    else if(number >= 0.33 && number < 0.66){
+        return "paper"
+    }
+    else if(number >= 0.66){
+        return "scissors"
+    }
+}
+
+function getHumanChoice(){
+    answer = prompt("Enter rock , paper or scissors to play")
+    return answer
+}
+
+console.log(getComputerChoice())
+console.log(getHumanChoice())
